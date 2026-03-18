04b60bb08f4994daef395bf3638aae31b17bf50d
BryanCabrera24 <147127942+dev-bryancabrera@users.noreply.github.com>
2026-03-17 22:41:52 -0500

    depuracion errores login 2

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/auth.store';
 
-console.log('API URL:', import.meta.env.VITE_API_URL);
-
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api',
+  baseURL: 'https://nfc-nexus-back-production.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
